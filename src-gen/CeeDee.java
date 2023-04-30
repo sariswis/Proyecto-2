4714c4ddf50304c77f80872a715889e623a3408d
sariswis <ss.cardenas@uniandes.edu.co>
2023-04-30 15:50:16 -0500

    reconoce cadenas

--- src-gen/CeeDee.java
+++ src-gen/CeeDee.java
@@ -55,18 +55,20 @@ public final class CeeDee {
     int $line=0;
     Object $result=null;
     $try:try {
-      $line=28;
+      $line=24;
       java.util.Scanner sc=null;
       sc=$invokeConstructor(java.util.Scanner.class,new Object[]{$invokeField(System.class,"in",true,null)});
-      $line=30;
+      $line=25;
       A=$fix($opIntvlY('a','z'));
-      $line=31;
+      $line=26;
       B=$fix($opIntvlY('0','9'));
-      $line=32;
+      $line=27;
       Object coder=$fix(CeeDee.createCoder());
-      $line=33;
+      $line=28;
       Object deCoder=$fix(CeeDee.createDecoder());
-      $line=40;
+      $line=29;
+      GAutomataFrame.show($cast(gold.structures.automaton.IAutomaton.class,coder));
+      $line=31;
       Utilities.testCodeDecode($cast(gold.structures.automaton.ITransducer.class,coder),$cast(gold.structures.automaton.ITransducer.class,deCoder));
     }
     catch (Throwable $throwable) {
@@ -77,7 +79,7 @@ public final class CeeDee {
     int $line=0;
     Object $result=null;
     $try:try {
-      $line=51;
+      $line=35;
       gold.structures.set.ISet $v1=$newSet();
       $v2:for (Object d:GCollections.unmodifiableCollection(GCollections.asSet(3,2,5))) {
         for (Object s:GCollections.unmodifiableCollection($opIntvlY('a','z'))) {
@@ -89,13 +91,13 @@ public final class CeeDee {
         }
       }
       Object Q=$fix($opUnionY($opUnionY($v1,GCollections.asSet(3,2,5,"I","E")),$opCartsY(new Object[]{GCollections.asSet(3,2,5),$opIntvlY('a','z')})));
-      $line=53;
+      $line=36;
       Object \u03A3=$fix($opUnionY($opIntvlY('a','z'),GCollections.asSet(':','2','3','5')));
-      $line=54;
+      $line=37;
       Object \u03A3_=$fix($opUnionY(\u03A3,GCollections.asSet('#')));
-      $line=55;
+      $line=38;
       Object q_0=$fix("I");
-      $line=56;
+      $line=39;
       gold.structures.set.ISet $v5=$newSet();
       $v6:for (Object d:GCollections.unmodifiableCollection(GCollections.asSet(3,2,5))) {
         for (Object s:GCollections.unmodifiableCollection($opIntvlY('a','z'))) {
@@ -107,10 +109,10 @@ public final class CeeDee {
         }
       }
       Object F=$fix($v5);
-      $line=58;
+      $line=40;
       $result=$invokeConstructor(GDeterministicTransducer.class,new Object[]{Q,\u03A3,\u03A3_,q_0,F,new GMethod(CeeDee.class,"\u03B4"),new GMethod(CeeDee.class,"g"),new GMethod(CeeDee.class,"h")});
       if (true) break $try;
-      $line=59;
+      $line=41;
       $rethrow(new RuntimeException("The function \"createCoder()\" did not return a value."));
     }
     catch (Throwable $throwable) {
@@ -122,49 +124,49 @@ public final class CeeDee {
     int $line=0;
     Object $result=null;
     $try:try {
-      $line=66;
+      $line=44;
       if (($opEqualY(state,"I")&&$opMembrY(input,GCollections.asSet('2','3','5')))) {
-        $line=66;
+        $line=44;
         $result=$opSubtrY(input,'0');
         if (true) break $try;
       }
       else {
-        $line=67;
+        $line=45;
         if (($opMembrY(state,GCollections.asSet(2,3,5))&&$opMembrY(input,$opIntvlY('a','z')))) {
-          $line=67;
+          $line=45;
           $result=GCollections.asList(state,input);
           if (true) break $try;
         }
         else {
-          $line=68;
+          $line=46;
           if ($opMembrY(state,GCollections.asSet(2,3,5,"I","E"))) {
-            $line=68;
+            $line=46;
             $result="E";
             if (true) break $try;
           }
           else {
-            $line=69;
+            $line=47;
             if (($opEqualY($opAbsolY(state),2)&&$opEqualY(input,':'))) {
-              $line=69;
+              $line=47;
               $result=GCollections.asList($getArrayValue(state,new Object[]{0}),$getArrayValue(state,new Object[]{1}),1);
               if (true) break $try;
             }
             else {
-              $line=70;
+              $line=48;
               if (($opEqualY($opAbsolY(state),3)&&$opMembrY(input,$opIntvlY('a','z')))) {
-                $line=71;
+                $line=49;
                 Object $v9=$getArrayValue(state,new Object[]{0});
                 Object $v10=$getArrayValue(state,new Object[]{1});
                 Object $v11=$getArrayValue(state,new Object[]{2});
                 Object d=$fix($v9);
                 Object s=$fix($v10);
                 Object pos=$fix($v11);
-                $line=72;
+                $line=50;
                 $result=GCollections.asList(d,s,$opModulY($opAdditY(pos,1),d));
                 if (true) break $try;
               }
               else {
-                $line=73;
+                $line=51;
                 $result="E";
                 if (true) break $try;
               }
@@ -172,7 +174,7 @@ public final class CeeDee {
           }
         }
       }
-      $line=76;
+      $line=53;
       $rethrow(new RuntimeException("The function \"\u03B4(state:Object,input:Object)\" did not return a value."));
     }
     catch (Throwable $throwable) {
@@ -184,65 +186,65 @@ public final class CeeDee {
     int $line=0;
     Object $result=null;
     $try:try {
-      $line=84;
+      $line=56;
       if (($opEqualY(state,"I")&&$opMembrY(input,GCollections.asSet('2','3','5')))) {
-        $line=84;
+        $line=56;
         $result=input;
         if (true) break $try;
       }
       else {
-        $line=85;
+        $line=57;
         if (($opMembrY(state,GCollections.asSet(2,3,5))&&$opMembrY(input,$opIntvlY('a','z')))) {
-          $line=85;
+          $line=57;
           $result=input;
           if (true) break $try;
         }
         else {
-          $line=86;
+          $line=58;
           if ($opMembrY(state,GCollections.asSet(2,3,5,"I","E"))) {
-            $line=86;
+            $line=58;
             $result="";
             if (true) break $try;
           }
           else {
-            $line=87;
+            $line=59;
             if (($opEqualY($opAbsolY(state),2)&&$opEqualY(input,':'))) {
-              $line=87;
+              $line=59;
               $result=":";
               if (true) break $try;
             }
             else {
-              $line=88;
+              $line=60;
               if (($opEqualY($opAbsolY(state),3)&&$opMembrY(input,$opIntvlY('a','z')))) {
-                $line=89;
+                $line=61;
                 Object $v12=$getArrayValue(state,new Object[]{0});
                 Object $v13=$getArrayValue(state,new Object[]{1});
                 Object $v14=$getArrayValue(state,new Object[]{2});
                 Object d=$fix($v12);
                 Object s=$fix($v13);
                 Object pos=$fix($v14);
-                $line=90;
+                $line=62;
                 if (($opEqualY(0,pos)&&$opEqualY(input,s))) {
-                  $line=90;
+                  $line=62;
                   $result="#";
                   if (true) break $try;
                 }
                 else {
-                  $line=91;
+                  $line=63;
                   if ($opEqualY(pos,1)) {
-                    $line=91;
+                    $line=63;
                     $result=CeeDee.shiftSymbol(input,d);
                     if (true) break $try;
                   }
                   else {
-                    $line=92;
+                    $line=64;
                     $result=input;
                     if (true) break $try;
                   }
                 }
               }
               else {
-                $line=94;
+                $line=66;
                 $result="";
                 if (true) break $try;
               }
@@ -250,7 +252,7 @@ public final class CeeDee {
           }
         }
       }
-      $line=97;
+      $line=68;
       $rethrow(new RuntimeException("The function \"h(state:Object,input:Object)\" did not return a value."));
     }
     catch (Throwable $throwable) {
@@ -262,10 +264,10 @@ public final class CeeDee {
     int $line=0;
     Object $result=null;
     $try:try {
-      $line=104;
+      $line=71;
       $result="";
       if (true) break $try;
-      $line=105;
+      $line=72;
       $rethrow(new RuntimeException("The function \"g(s:Object)\" did not return a value."));
     }
     catch (Throwable $throwable) {
@@ -277,7 +279,7 @@ public final class CeeDee {
     int $line=0;
     Object $result=null;
     $try:try {
-      $line=108;
+      $line=75;
       gold.structures.set.ISet $v15=$newSet();
       $v16:for (Object d:GCollections.unmodifiableCollection(GCollections.asSet(3,2,5))) {
         for (Object s:GCollections.unmodifiableCollection($opIntvlY('a','z'))) {
@@ -287,13 +289,13 @@ public final class CeeDee {
         }
       }
       Object Q=$fix($opUnionY($v15,GCollections.asSet(3,2,5,"I","E")));
-      $line=109;
+      $line=76;
       Object \u03A3_=$fix($opUnionY($opIntvlY('a','z'),GCollections.asSet(':','2','3','5')));
-      $line=110;
+      $line=77;
       Object \u03A3=$fix($opUnionY(\u03A3_,GCollections.asSet('#')));
-      $line=111;
+      $line=78;
       Object q_0=$fix("I");
-      $line=112;
+      $line=79;
       gold.structures.set.ISet $v17=$newSet();
       $v18:for (Object d:GCollections.unmodifiableCollection(GCollections.asSet(3,2,5))) {
         for (Object s:GCollections.unmodifiableCollection($opIntvlY('a','z'))) {
@@ -305,10 +307,10 @@ public final class CeeDee {
         }
       }
       Object F=$fix($v17);
-      $line=114;
+      $line=80;
       $result=$invokeConstructor(GDeterministicTransducer.class,new Object[]{Q,\u03A3,\u03A3_,q_0,F,new GMethod(CeeDee.class,"\u03B41"),new GMethod(CeeDee.class,"g"),new GMethod(CeeDee.class,"h1")});
       if (true) break $try;
-      $line=115;
+      $line=81;
       $rethrow(new RuntimeException("The function \"createDecoder()\" did not return a value."));
     }
     catch (Throwable $throwable) {
@@ -320,26 +322,26 @@ public final class CeeDee {
     int $line=0;
     Object $result=null;
     $try:try {
-      $line=121;
+      $line=84;
       if (($opEqualY(state,"I")&&$opMembrY(input,GCollections.asSet('2','3','5')))) {
-        $line=121;
+        $line=84;
         $result=$opSubtrY(input,'0');
         if (true) break $try;
       }
       else {
-        $line=122;
+        $line=85;
         if (($opMembrY(state,GCollections.asSet(2,3,5))&&$opMembrY(input,$opIntvlY('a','z')))) {
-          $line=122;
+          $line=85;
           $result=GCollections.asList(state,input,"_");
           if (true) break $try;
         }
         else {
-          $line=123;
+          $line=86;
           $result="E";
           if (true) break $try;
         }
       }
-      $line=125;
+      $line=88;
       $rethrow(new RuntimeException("The function \"\u03B41(state:Object,input:Object)\" did not return a value."));
     }
     catch (Throwable $throwable) {
@@ -355,42 +357,42 @@ public final class CeeDee {
       Object d=$v22.next();
       Object s=$v22.next();
       Object pos=$v22.next();
-      $line=128;
+      $line=91;
       if (($opEqualY(pos,"_")&&$opEqualY(input,':'))) {
-        $line=128;
+        $line=91;
         $result=GCollections.asList(d,s,1);
         if (true) break $try;
       }
       else {
-        $line=129;
+        $line=92;
         if ($opEqualY(pos,"_")) {
-          $line=129;
+          $line=92;
           $result="E";
           if (true) break $try;
         }
         else {
-          $line=130;
+          $line=93;
           if (($opEqualY(input,'#')&&!$opEqualY(pos,0))) {
-            $line=130;
+            $line=93;
             $result="E";
             if (true) break $try;
           }
           else {
-            $line=131;
+            $line=94;
             if (($opEqualY(input,s)&&$opEqualY(pos,0))) {
-              $line=131;
+              $line=94;
               $result="E";
               if (true) break $try;
             }
             else {
-              $line=132;
+              $line=95;
               if ($opMembrY(input,$opUnionY($opIntvlY('a','z'),GCollections.asSet('#')))) {
-                $line=132;
+                $line=95;
                 $result=GCollections.asList(d,s,$opModulY($opAdditY(pos,1),d));
                 if (true) break $try;
               }
               else {
-                $line=133;
+                $line=96;
                 $result="E";
                 if (true) break $try;
               }
@@ -398,7 +400,7 @@ public final class CeeDee {
           }
         }
       }
-      $line=135;
+      $line=98;
       $rethrow(new RuntimeException("The function \"\u03B41(\u27E8d,s,pos\u27E9:java.lang.Iterable,input:Object)\" did not return a value."));
     }
     catch (Throwable $throwable) {
@@ -410,26 +412,26 @@ public final class CeeDee {
     int $line=0;
     Object $result=null;
     $try:try {
-      $line=141;
+      $line=101;
       if (($opEqualY(state,"I")&&$opMembrY(input,GCollections.asSet('2','3','5')))) {
-        $line=141;
+        $line=101;
         $result=$opSubtrY(input,'0');
         if (true) break $try;
       }
       else {
-        $line=142;
+        $line=102;
         if (($opMembrY(state,GCollections.asSet(2,3,5))&&$opMembrY(input,$opIntvlY('a','z')))) {
-          $line=142;
+          $line=102;
           $result=input;
           if (true) break $try;
         }
         else {
-          $line=143;
+          $line=103;
           $result="";
           if (true) break $try;
         }
       }
-      $line=145;
+      $line=105;
       $rethrow(new RuntimeException("The function \"h1(state:Object,input:Object)\" did not return a value."));
     }
     catch (Throwable $throwable) {
@@ -445,42 +447,42 @@ public final class CeeDee {
       Object d=$v24.next();
       Object s=$v24.next();
       Object pos=$v24.next();
-      $line=148;
+      $line=108;
       if (($opEqualY(pos,"_")&&$opEqualY(input,':'))) {
-        $line=148;
+        $line=108;
         $result=input;
         if (true) break $try;
       }
       else {
-        $line=149;
+        $line=109;
         if (($opEqualY(input,'#')&&$opEqualY(pos,0))) {
-          $line=149;
+          $line=109;
           $result=s;
           if (true) break $try;
         }
         else {
-          $line=150;
+          $line=110;
           if (($opMembrY(input,$opIntvlY('a','z'))&&$opEqualY(pos,1))) {
-            $line=150;
+            $line=110;
             $result=CeeDee.shiftSymbol(input,$opMinusY(d));
             if (true) break $try;
           }
           else {
-            $line=151;
+            $line=111;
             if ($opMembrY(input,$opIntvlY('a','z'))) {
-              $line=151;
+              $line=111;
               $result=input;
               if (true) break $try;
             }
             else {
-              $line=152;
+              $line=112;
               $result="";
               if (true) break $try;
             }
           }
         }
       }
-      $line=154;
+      $line=114;
       $rethrow(new RuntimeException("The function \"h1(\u27E8d,s,pos\u27E9:java.lang.Iterable,input:Object)\" did not return a value."));
     }
     catch (Throwable $throwable) {
@@ -492,36 +494,36 @@ public final class CeeDee {
     int $line=0;
     Object $result=null;
     $try:try {
-      $line=162;
+      $line=117;
       Character r=null;
       r=$defaultValue(Character.class);
-      $line=164;
+      $line=118;
       if ($opMembrY(symbol,$opIntvlY('a','z'))) {
-        $line=165;
+        $line=119;
         r=$cast(Character.class,$fix($opAdditY('a',$opModulY($opAdditY($opSubtrY(symbol,'a'),shift),26))));
       }
       else {
-        $line=166;
+        $line=120;
         if ($opMembrY(symbol,$opIntvlY('A','Z'))) {
-          $line=167;
+          $line=121;
           r=$cast(Character.class,$fix($opAdditY('A',$opModulY($opAdditY($opSubtrY(symbol,'A'),shift),26))));
         }
         else {
-          $line=168;
+          $line=122;
           if ($opMembrY(symbol,$opIntvlY('0','9'))) {
-            $line=169;
+            $line=123;
             r=$cast(Character.class,$fix($opAdditY('0',$opModulY($opAdditY($opSubtrY(symbol,'0'),shift),10))));
           }
           else {
-            $line=170;
+            $line=124;
             r=$cast(Character.class,$fix(r));
           }
         }
       }
-      $line=172;
+      $line=126;
       $result=r;
       if (true) break $try;
-      $line=173;
+      $line=127;
       $rethrow(new RuntimeException("The function \"shiftSymbol(symbol:Object,shift:Object)\" did not return a value."));
     }
     catch (Throwable $throwable) {
